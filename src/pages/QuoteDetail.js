a3ee12f53665ff19e2ff50205c3c656e09286ee4
Mahesh Mahajan <mahajanmahesh50@gmail.com>
2022-12-19 11:52:02 +0530

    Lets use nested route feature

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,11 +1,17 @@
-import { useParams } from "react-router-dom";
+import { useParams, Route } from "react-router-dom";
+
+import Comments from "../components/comments/Comments";
+
 const QuoteDetail = () => {
   const params = useParams();
 
   return (
     <>
-      <h1>Quoute Details page.</h1>
-      <p>Here is the Id #{params.quoteId}</p>
+      <h1>Quote Detail Page</h1>
+      <p>{params.quoteId}</p>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
     </>
   );
 };
