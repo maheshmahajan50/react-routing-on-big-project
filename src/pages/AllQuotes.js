2c4cffce25235c6bcf2d0453d3a30dc43ef2db65
Mahesh Mahajan <mahajanmahesh50@gmail.com>
2022-12-19 12:21:40 +0530

    Adding dummy data and more content

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,16 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Mahesh", text: "Learning react is fun!!" },
+  { id: "q2", author: "Mahajan", text: "Learning react is Great!!" },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quoutes page.</h1>;
+  return (
+    <>
+      <QuoteList quotes={DUMMY_QUOTES} />
+    </>
+  );
 };
 
 export default AllQuotes;
