fc49ee657492495da3f82a78848e30ae9ce286fb
Mahesh Mahajan <mahajanmahesh50@gmail.com>
2022-12-19 12:36:10 +0530

    Outputting data on details page.

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,14 +1,24 @@
 import { useParams, Route } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Mahesh", text: "Learning react is fun!!" },
+  { id: "q2", author: "Mahajan", text: "Learning react is Great!!" },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No Quote Found !!</p>;
+  }
 
   return (
     <>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote author={quote.author} text={quote.text} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
